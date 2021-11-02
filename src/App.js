697e279777b8ed32080be47ad2e64292c4303374
layla4131 <layla4131@gmail.com>
2021-11-03 02:38:04 +0800

    新增booking_index
    新增booling_index
    以及連結哲聖的index首頁
    套用Footer版型

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import Footer from "./components/Footer.js";
+import index from "./pages/discuss/index.js";
+import booking_index from "./pages/booking_pages/booking_index.js";
 import { HashRouter, Route, Switch, Link } from "react-router-dom";
-
 
 function App() {
   return (
@@ -12,6 +14,11 @@ function App() {
         <HashRouter>
           <Switch>
             <Route exact={true} path="/" component={index} />
+            <Route
+              exact={true}
+              path="/booking_index"
+              component={booking_index}
+            />
           </Switch>
         </HashRouter>
       </div>
