158551b75d9f7d3dee74c62138aea2cb82d8b5d6
layla4131 <layla4131@gmail.com>
2021-11-03 20:48:41 +0800

    排版更新
    新增
    1.場地租借日曆
    2.照片輪播
    3.場地照片
    4.排版更新

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Footer from "./components/Footer.js";
 import index from "./pages/discuss/index.js";
-import booking_index from "./pages/booking_pages/booking_index.js";
+import booking from "./pages/booking_pages/booking_index.js";
 import { HashRouter, Route, Switch, Link } from "react-router-dom";
 
 function App() {
@@ -14,14 +14,11 @@ function App() {
         <HashRouter>
           <Switch>
             <Route exact={true} path="/" component={index} />
-            <Route
-              exact={true}
-              path="/booking_index"
-              component={booking_index}
-            />
+            <Route exact={true} path="/booking" component={booking} />
           </Switch>
         </HashRouter>
       </div>
+
       <Footer />
     </>
   );
