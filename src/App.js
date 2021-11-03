20878e50fbc07f87e56c847ba01ee92fa81c5d87
Samyo <love51213@gmail.com>
2021-11-03 16:57:38 +0800

    app.js解merge衝突

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Switch, Link } from "react-router-dom";
@@ -6,13 +7,22 @@ import TopNavbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="boradWidth">
-      <HashRouter>
-        <Switch>
-          <Route exact={true} path="/" component={TopNavbar} />
-        </Switch>
-      </HashRouter>
-    </div>
+    <>
+      <TopNavbar />
+      <div className="bodyWidth">
+        <HashRouter>
+          <Switch>
+            <Route exact={true} path="/" component={index} />
+            <Route
+              exact={true}
+              path="/booking_index"
+              component={booking_index}
+            />
+          </Switch>
+        </HashRouter>
+      </div>
+      <Footer />
+    </>
   );
 }
 
