4d867fe762ef1711f8284e4aa62421e611bf00b4
Ya-Wen,Chan <89444541+YawenChan@users.noreply.github.com>
2021-11-04 19:58:49 +0800

    新增標籤、可以報名的按鈕

--- src/pages/contest/contest_index.js
+++ src/pages/contest/contest_index.js
@@ -12,38 +12,57 @@ import {Container, Row,Col,} from 'react-bootstrap'
 function index(props) {
     return (
         <>
-        {/* <div className="p-3">
-            <h2 className=""> <FontAwesomeIcon icon={faDice}  />活動資訊</h2> 
-        </div>
-        <div className="line" /> */}
         
+        {/* 活動資訊 */}
         <h2 className="text-center">活動資訊</h2>
-      <div className="titleLineBox">
-        <img alt="" className="titleLine" src="img/index/line.png" />
-      </div>
+        <div className="titleLineBox">
+            <img alt="" className="titleLine" src="img/index/line.png" />
+        </div>
 
-        <div className="searchBar m-3 p-2 ">
-        {/* <div className="searchBarWhite p-2"></div> */}
+    <div className="searchBar ">
+        <div className="d-inline">
             <form action="" className="d-inline">
-            <input type="search" className="p-2"/>
-            <span className="searchIcon"></span>
-                <input type="checkbox" className="" value="" />
-                <label For="vehicle1" className="fs-3 fontColor">卡牌</label>
-                <input type="checkbox" className="" value=""/>
-                <label for="vehicle2" className="fs-3 fontColor">家庭</label>
-                <input type="checkbox" className="" value=""/>
-                <label for="vehicle3" className="fs-3 fontColor"> 策略</label>
-                <label className="fs-3 fontColor">
-                <span className="delete">&#10005;</span>清除結果</label>
+                <input type="search" className=""/>
             </form> 
-            
+        </div>
+        <div>
+            <form action="" className="d-inline">
+            <input type="checkbox" className="" value="" id="card" />
+                <label For="card" className="fs-3 fontColor">卡牌</label>
+                <input type="checkbox" className="" value="" id="family"/>
+                <label for="family" className="fs-3 fontColor">家庭</label>
+                <input type="checkbox" className="" value="" id="stategy"/>
+                <label for="stategy" className="fs-3 fontColor"> 策略</label>
+                <label className="fs-3 fontColor">
+                    <span className="deleButton">&#10005; 清除結果</span>
+                </label>
+            </form>
+        </div>
+    </div>
         
-      
-     </div>
+                
+              
+    
+
+     {/* 活動資訊卡片 */}
      <Container>
         <Row>
             <Col md={6}>
+                
                 <div className="card m-5">
+                {/* 標籤 */}
+                <a
+                  href="#/"
+                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
+                >
+                  <img
+                    alt=""
+                    src="img/index/card-tag.png"
+                    className="tagImg"
+                  />
+                  <p className="tagText">卡牌</p>
+                </a>
+                {/* 內容 */}
                     <div className="contestPic">
                     <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/c01.jpg'}  fluid />
                     </div>
@@ -59,7 +78,52 @@ function index(props) {
                 </div>
             </Col>
             <Col md={6}>
+            
+            
+            {/* 內容 */}
                 <div className="card m-5">
+                    {/* 標籤 */}
+                <a
+                  href="#/"
+                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
+                >
+                  <img
+                    alt=""
+                    src="img/index/trag-tag.png"
+                    className="tagImg"
+                  />
+                  <p className="tagText">策略</p>
+                </a>
+                {/* 內容 */}
+                    <div className="contestPic">
+                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/c01.jpg'} background-size="cover" fluid />
+                    </div>
+                    <p className="fs-5 m-2">2021-12-25(六)</p>
+                    <h5 className="">《寶可夢王者挑戰賽》</h5>
+                    <p className="p-2 m-2">大家期待已久的寶可夢卡牌對戰賽終於來啦!<br/>
+                                        趕快手刀衝刺來報名吧!</p>
+                    <div className="d-flex">
+                         <div className="personNo">剩餘名額:3位</div>
+                        <div className="doButton">我要報名</div>
+                </div>
+                    
+                </div>
+            </Col>
+            <Col md={6}>
+                <div className="card m-5">
+                    {/* 標籤 */}
+                    <a
+                  href="#/"
+                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
+                >
+                  <img
+                    alt=""
+                    src="img/index/family-tag.png"
+                    className="tagImg"
+                  />
+                  <p className="tagText">家庭</p>
+                </a>
+                {/* 內容 */}
                     <div className="contestPic">
                     <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/c01.jpg'} background-size="cover" fluid />
                     </div>
@@ -76,6 +140,48 @@ function index(props) {
             </Col>
             <Col md={6}>
                 <div className="card m-5">
+                    {/* 標籤 */}
+                    <a
+                  href="#/"
+                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
+                >
+                  <img
+                    alt=""
+                    src="img/index/card-tag.png"
+                    className="tagImg"
+                  />
+                  <p className="tagText">卡牌</p>
+                </a>
+                {/* 內容 */}
+                    <div className="contestPic">
+                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/c01.jpg'} background-size="cover" fluid />
+                    </div>
+                    <p className="fs-5 m-2">2021-12-25(六)</p>
+                    <h5 className="">《寶可夢王者挑戰賽》</h5>
+                    <p className="p-2 m-2">大家期待已久的寶可夢卡牌對戰賽終於來啦!<br/>
+                                        趕快手刀衝刺來報名吧!</p>
+                    <div className="d-flex">
+                         <div className="personNo">剩餘名額:10位</div>
+                        <div className="doButton">我要報名</div>
+                </div>
+                    
+                </div>
+            </Col>
+            <Col md={6}>
+                <div className="card m-5">
+                    {/* 標籤 */}
+                    <a
+                  href="#/"
+                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
+                >
+                  <img
+                    alt=""
+                    src="img/index/trag-tag.png"
+                    className="tagImg"
+                  />
+                  <p className="tagText">策略</p>
+                </a>
+                {/* 內容 */}
                     <div className="contestPic">
                     <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/c01.jpg'} background-size="cover" fluid />
                     </div>
@@ -92,38 +198,19 @@ function index(props) {
             </Col>
             <Col md={6}>
                 <div className="card m-5">
-                    <div className="contestPic">
-                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/c01.jpg'} background-size="cover" fluid />
-                    </div>
-                    <p className="fs-5 m-2">2021-12-25(六)</p>
-                    <h5 className="">《寶可夢王者挑戰賽》</h5>
-                    <p className="p-2 m-2">大家期待已久的寶可夢卡牌對戰賽終於來啦!<br/>
-                                        趕快手刀衝刺來報名吧!</p>
-                    <div className="d-flex">
-                         <div className="personNo">剩餘名額:0位</div>
-                        <div className="disButton">已額滿</div>
-                </div>
-                    
-                </div>
-            </Col>
-            <Col md={6}>
-                <div className="card m-5">
-                    <div className="contestPic">
-                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/c01.jpg'} background-size="cover" fluid />
-                    </div>
-                    <p className="fs-5 m-2">2021-12-25(六)</p>
-                    <h5 className="">《寶可夢王者挑戰賽》</h5>
-                    <p className="p-2 m-2">大家期待已久的寶可夢卡牌對戰賽終於來啦!<br/>
-                                        趕快手刀衝刺來報名吧!</p>
-                    <div className="d-flex">
-                         <div className="personNo">剩餘名額:0位</div>
-                        <div className="disByautton">已額滿</div>
-                </div>
-                    
-                </div>
-            </Col>
-            <Col md={6}>
-                <div className="card m-5">
+                        {/* 標籤 */}
+                <a
+                  href="#/"
+                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
+                >
+                  <img
+                    alt=""
+                    src="img/index/family-tag.png"
+                    className="tagImg"
+                  />
+                  <p className="tagText">家庭</p>
+                </a>
+                {/* 內容 */}
                     <div className="contestPic">
                     <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/c01.jpg'} background-size="cover" fluid />
                     </div>
