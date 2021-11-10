ab849808a242704e60c66118d949dcc5d36e6525
Ya-Wen,Chan <89444541+YawenChan@users.noreply.github.com>
2021-11-10 23:42:42 +0800

    將活動資訊頁改成json資料帶入

--- src/pages/contest/contest_index.js
+++ src/pages/contest/contest_index.js
@@ -1,13 +1,90 @@
-import React from 'react'
-//import PropTypes from 'prop-types'
+import {React,useState} from 'react'
+import PropTypes from 'prop-types'
 import "../../css/contest.css";
 import "normalize.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from '@fortawesome/free-solid-svg-icons' // <-- import faSearch
 import {Container, Row,Col,Form,FormControl,Button} from "react-bootstrap";
-//import Background from '../../../public/img/contest/conBg.jpg'
+import ContestCard from '../../components/contest/ContestCard';
+
+// 假資料製作
+const contestItems = [
+  {
+  id: 1,
+  date:'2021-12-25(六)',
+  title:'《寶可夢王者挑戰賽》',
+  innertext:'大家期待已久的寶可夢卡牌對戰賽終於來啦!趕快手刀衝刺來報名吧!',
+  limit: 2,
+  category:'卡牌',
+  img:'img/contest/con01.jpg',
+  imgTag:'img/index/card-tag.png',
+  },
+  {
+  id: 2,
+  date:'2021-12-10(五)',
+  title:'《辣個蟑螂又來啦!》',
+  innertext:'非常耐玩的德國蟑螂要在遊戲職人舉辦比賽了，來挑戰看看吧!',
+  limit: 10,
+  category:'卡牌',
+  img:'img/contest/con02.jpg',
+  imgTag:"img/index/card-tag.png",
+  },
+  {
+  id: 3,
+  date:'2021-11-28(日)',
+  title:'《要...一起跳舞嗎?》',
+  innertext:'犯人在跳舞，你抓得到嗎?一款鬥心機的卡牌遊戲，來參加看看嗎?',
+  limit: 5,
+  category:'卡牌',
+  img:'img/contest/con03.jpg',
+  imgTag:'img/index/card-tag.png',
+  },
+  {
+    id: 4,
+    date:'2021-11-15(一)',
+    title:'《波多黎各，渡假溜~》',
+    innertext:'有玩過波多璃各嗎?高手們快來報名參加吧!',
+    limit: 5,
+    category:'策略',
+    img:'img/contest/con04.jpg',
+    imgTag:'img/index/trag-tag.png',
+    },
+    {
+      id:5,
+      date:'2021-10-31(日)',
+      title:'《愜意午後的農家樂!》',
+      innertext:'嚮往農村生活嗎?快揪三五好友一同來比賽，渡過一個愉快的下午吧~',
+      limit: 5,
+      category:'策略',
+      img:'img/contest/con05.jpg',
+      imgTag:'img/index/trag-tag.png',
+      },
+    {
+      id: 6,
+      date:'2021-10-25(一)',
+      title:'《大富翁全家同樂會》',
+      innertext:'歡迎來參加活動',
+      limit: 5,
+      category:'家庭',
+      img:'img/contest/con06.jpg',
+      imgTag:'img/index/family-tag.png',
+    },
+]
+ 
+// 使用迴圈將json檔寫入contestRun的常數中做為一個陣列
+const contestRun = (c)=>{
+  const state= []  // 做一個空陣列
+  for (let i = 0; i < c.length; i++) {
+    state.push({...c[i]})
+  }
+  return state
+}
+
 function Index(props) {
+  const [contest, setContest] = useState(
+    contestRun(contestItems)
+  )
     return (
       <>
         {/* 活動資訊 */}
@@ -48,189 +125,29 @@ function Index(props) {
     </div>
         
     <div>
+    
+    {/* <BarChart/> */}
     <img src="../img/contest/conBg01.png" alt="" className="bagd" />
        {/* 活動資訊卡片 */}
      <Container >
         <Row>
-            <Col md={6}>
-                
-                <div className="card m-5">
-                {/* 標籤 */}
-                <a
-                  href="#/"
-                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
-                >
-                  <img
-                    alt=""
-                    src="img/index/family-tag.png"
-                    className="tagImg"
-                  />
-                  <p className="tagText">家庭</p>
-                </a>
-                {/* 內容 */}
-                    <div className="contestPic">
-                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/con01.jpg'}  fluid />
-                    </div>
-                    <p className="fs-5 m-2">2021-12-25(六)</p>
-                    <h5>《寶可夢王者挑戰賽》</h5>
-                    <p className="p-2 m-2">大家期待已久的寶可夢卡牌對戰賽終於來啦!<br/>
-                                        趕快手刀衝刺來報名吧!</p>
-                    <div className="d-flex">
-                         <div className="personNo">剩餘名額:0位</div>
-                        <div className="disButton ">已額滿</div>
-                </div>
-                    
-                </div>
-            </Col>
-            <Col md={6}>
-            
-            
-            {/* 內容 */}
-                <div className="card m-5">
-                    {/* 標籤 */}
-                <a
-                  href="#/"
-                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
-                >
-                  <img
-                    alt=""
-                    src="img/index/card-tag.png"
-                    className="tagImg"
-                  />
-                  <p className="tagText">卡牌</p>
-                </a>
-                {/* 內容 */}
-                    <div className="contestPic">
-                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/con02.jpg'} background-size="cover" fluid />
-                    </div>
-                    <p className="fs-5 m-2">2021-12-10(五)</p>
-                    <h5>《辣個蟑螂又來啦!》</h5>
-                    <p className="p-2 m-2">非常耐玩的德國蟑螂要在遊戲職人<br/>
-                      舉辦比賽了，來挑戰看看吧!</p>
-                    <div className="d-flex">
-                        <div className="personNo">剩餘名額:6位</div>
-                        <a className="doButton" href="contestInfo">我要報名</a>
-                </div>
-                    
-                </div>
-            </Col>
-            <Col md={6}>
-                <div className="card m-5">
-                    {/* 標籤 */}
-                    <a
-                  href="#/"
-                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
-                >
-                  <img
-                    alt=""
-                    src="img/index/card-tag.png"
-                    className="tagImg"
-                  />
-                  <p className="tagText">卡牌</p>
-                </a>
-                {/* 內容 */}
-                    <div className="contestPic">
-                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/con03.jpg'} background-size="cover" fluid />
-                    </div>
-                    <p className="fs-5 m-2">2021-11-28(日)</p>
-                    <h5>《要...一起跳舞嗎?》</h5>
-                    <p className="p-2 m-2">犯人在跳舞，你抓得到嗎?<br/>
-                        一款鬥心機的卡牌遊戲，來參加看看嗎?</p>
-                    <div className="d-flex">
-                         <div className="personNo">剩餘名額:2位</div>
-                        <div className="doButton">我要報名</div>
-                </div>
-                    
-                </div>
-            </Col>
-            <Col md={6}>
-                <div className="card m-5">
-                    {/* 標籤 */}
-                    <a
-                  href="#/"
-                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
-                >
-                  <img
-                    alt=""
-                    src="img/index/trag-tag.png"
-                    className="tagImg"
-                  />
-                  <p className="tagText">策略</p>
-                </a>
-                {/* 內容 */}
-                    <div className="contestPic">
-                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/con04.jpg'} background-size="cover" fluid />
-                    </div>
-                    <p className="fs-5 m-2">2021-11-15(一)</p>
-                    <h5>《波多黎各，渡假溜~》</h5>
-                    <p className="p-2 m-2">有玩過波多璃各嗎?<br/>
-                        高手們快來報名參加吧!</p>
-                    <div className="d-flex">
-                         <div className="personNo">剩餘名額:2位</div>
-                        <div className="doButton">我要報名</div>
-                </div>
-                    
-                </div>
-            </Col>
-            <Col md={6}>
-                <div className="card m-5">
-                    {/* 標籤 */}
-                    <a
-                  href="#/"
-                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
-                >
-                  <img
-                    alt=""
-                    src="img/index/trag-tag.png"
-                    className="tagImg"
-                  />
-                  <p className="tagText">策略</p>
-                </a>
-                {/* 內容 */}
-                    <div className="contestPic">
-                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/con05.jpg'} background-size="cover" fluid />
-                    </div>
-                    <p className="fs-5 m-2">2021-10-31(日)</p>
-                    <h5 className="">《愜意午後的農家樂!》</h5>
-                    <p className="p-2 m-2">嚮往農村生活嗎?<br/>
-                        快揪三五好友一同來比賽，渡過一個愉快的下午吧~</p>
-                    <div className="d-flex">
-                         <div className="personNo">剩餘名額:3位</div>
-                        <div className="doButton">我要報名</div>
-                </div>
-                    
-                </div>
-            </Col>
-            <Col md={6}>
-                <div className="card m-5">
-                        {/* 標籤 */}
-                <a
-                  href="#/"
-                  className="text-dark text-decoration-none recommendTag d-inline-block pt-1"
-                >
-                  <img
-                    alt=""
-                    src="img/index/family-tag.png"
-                    className="tagImg"
-                  />
-                  <p className="tagText">家庭</p>
-                </a>
-                {/* 內容 */}
-                    <div className="contestPic">
-                    <img alt="遊戲圖片" className="images" src={process.env.PUBLIC_URL + '/img/contest/con06.jpg'} background-size="cover" fluid />
-                    </div>
-                    <p className="fs-5 m-2">2021-10-25(一)</p>
-                    <h5 className="">《大富翁全家同樂會》</h5>
-                    <p className="p-2 m-2">闔家同樂的大富翁~<br/>
-                        適合攜家帶眷一同報名參加!</p>
-                    <div className="d-flex">
-                        <div className="personNo">剩餘名額:0位</div>
-                        <div className="disButton">已額滿</div>
-                    </div>
-                    
-                </div>
-            </Col>
-            
+        {contest.map((v,i) => {
+          return(
+            <ContestCard
+            date={v.date}
+            title={v.title}
+            innertext={v.innertext}
+            limit={v.limit}
+            category={v.category}
+            img={v.img}
+            imgTag={v.imgTag}
+          />
+          )
+        })}
+          
+          
+          
+           
         </Row>
 </Container>
 </div>
