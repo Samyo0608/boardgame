b791f9d08d2bb3719cf63e6fa15f5198619af740
kimpoiprince <kimpoiprince@gmail.com>
2021-11-11 18:02:05 +0800

    新增結帳頁面

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Form, Row, Col } from "react-bootstrap";
+import { Button, Image, Form, Row, Col } from "react-bootstrap";
 import "../../css/login.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationCircle } from "@fortawesome//free-solid-svg-icons";
@@ -9,17 +9,16 @@ function Login(props) {
   const [yellow, setYellow] = useState("");
   return (
     <div className="login">
-      <img
-        alt=""
+      <Image
         src="../../img/booking/booking_index.jpg"
         className="backgroundImageLogin"
       />
       <div className="d-flex flex-column CenterDivLogin justify-content-around align-items-center">
         <p className="h2 mt-5 bold">會員登入</p>
         <div className="titleLineBox">
-          <img alt="" className="titleLine" src="/img/index/line.png" />
+          <img alt="" className="titleLine" src="img/index/line.png" />
         </div>
-        <Form method="post">
+        <Form>
           <Form.Group as={Row} className="mb-4" controlId="formBasicEmail">
             <Form.Label column sm="4" className="bold">
               電子信箱
@@ -63,7 +62,7 @@ function Login(props) {
           </Button>
         </Form>
         <hr className="hrLogin" />
-        <Button variant="primary" className="mb-5 button" type="submit">
+        <Button variant="primary" className="mb-5 button">
           免費註冊新會員
         </Button>
       </div>
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,44 +1,13 @@
-/* eslint-disable no-unused-expressions */
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Image, Form, Row, Col } from "react-bootstrap";
 import "../../css/register.css";
-import axios from "axios";
-import { API_URL } from "../../configs/config";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationCircle } from "@fortawesome//free-solid-svg-icons";
 
 function Register(props) {
-  const [member, setMember] = useState({
-    email: "",
-    password: "",
-    rePassword: "",
-  });
-
-  // function handleSubmit(e) {
-  //   alert("成功");
-  // }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      alert("成功");
-      // let res = await axios.post(`${API_URL}/auth/register`, member, {
-      //   // 因為會讓 res cors 寫 cookie
-      //   withCredentials: true,
-      // });
-      await axios.post(`${API_URL}/auth/register`, member, {
-        // 因為會讓 res cors 寫 cookie
-        withCredentials: true,
-      });
-    } catch (e) {
-      console.log("handleSubmit", e);
-    }
-  }
-  const handleChange = (e) => {
-    let newMember = { ...member };
-    newMember[e.target.name] = e.target.value;
-    setMember(newMember);
-  };
-
+  const [yellow, setYellow] = useState("");
+  const [yellow2, setYellow2] = useState("");
   return (
     <div className="login">
       <Image
@@ -48,61 +17,70 @@ function Register(props) {
       <div className="d-flex flex-column CenterDiv justify-content-center align-items-center">
         <p className="h2 mt-5 bold">新會員註冊</p>
         <div className="titleLineBox">
-          <img alt="" className="titleLine" src="/img/index/line.png" />
+          <img alt="" className="titleLine" src="img/index/line.png" />
         </div>
-
-        <Form className="mb-5" method="post" onSubmit={handleSubmit}>
-          <Form.Group as={Row} className="mb-3" controlId="email">
-
+        <Form className="mb-5">
+          <Form.Group as={Row} className="mb-3" controlId="formBasicEmail">
             <Form.Label column sm="4" className="bold">
               電子信箱
             </Form.Label>
             <Col sm="8">
               <Form.Control
-                name="email"
                 type="email"
                 placeholder="請輸入email"
                 className="input"
-                required
-                value={member.email}
-                onChange={handleChange}
               />
             </Col>
           </Form.Group>
 
-          <Form.Group as={Row} className="mb-3" controlId="password">
+          <Form.Group as={Row} className="mb-3" controlId="">
+            <Form.Label column sm="4" className="bold">
+              使用者帳號
+            </Form.Label>
+            <Col sm="8">
+              <Form.Control
+                type="text"
+                placeholder="請輸入帳號"
+                className="input"
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3" controlId="formBasicPassword">
             <Form.Label column sm="4" className="bold">
               密碼
             </Form.Label>
             <Col sm="8">
               <Form.Control
-                name="password"
                 type="password"
                 placeholder="請輸入密碼"
-                minlength="8"
-                maxlength="20"
-                required
-                value={member.password}
-                className="input"
-                onChange={handleChange}
+                className={`${yellow} input`}
+                onChange={(e) => {
+                  if (e.target.value.length > 0) {
+                    setYellow("yellow");
+                  } else {
+                    setYellow("");
+                  }
+                }}
               />
             </Col>
           </Form.Group>
-          <Form.Group as={Row} className="mb-3" controlId="rePassword">
+          <Form.Group as={Row} className="mb-3" controlId="formBasicRePassword">
             <Form.Label column sm="4" className="bold">
               再次確認密碼
             </Form.Label>
             <Col sm="8">
               <Form.Control
-                name="rePassword"
                 type="password"
                 placeholder="再次確認密碼"
-                minlength="8"
-                maxlength="20"
-                required
-                value={member.rePassword}
-                className="input"
-                onChange={handleChange}
+                className={`${yellow2} input`}
+                onChange={(e) => {
+                  if (e.target.value.length > 0) {
+                    setYellow2("yellow");
+                  } else {
+                    setYellow2("");
+                  }
+                }}
               />
             </Col>
           </Form.Group>
@@ -112,13 +90,11 @@ function Register(props) {
           >
             <Form.Check
               type="CheckBox"
-              name="ness"
               label="我了解網站資訊並同意相關服務條款"
-              required
             />
           </Form.Group>
           <Button variant="primary" type="submit" className="mt-5 button">
-            註冊
+            登入
           </Button>
         </Form>
       </div>
