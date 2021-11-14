842d222a9701fcf6c9a8726b9ea0f32d8bef3430
Ya-Wen,Chan <89444541+YawenChan@users.noreply.github.com>
2021-11-14 13:21:06 +0800

    活動頁搜尋功能增加、修正vote頁barchart跑版

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,3 +1,23 @@
 export const API_URL = "http://localhost:3001/api";
 
 export const URL = "http://localhost:3001";
+
+export const labelImgs=[
+    {
+        category:"卡牌",
+        cardImg:"img/index/card-tag.png",
+
+    },
+    {
+        category:"家庭",
+        cardImg:"img/index/family-tag.png",
+
+    },
+    {
+        category:"策略",
+        cardImg:"img/index/trag-tag.png",
+
+    },
+
+
+]
