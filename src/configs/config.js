3b41afa5fbc678c308051cfac7b078fbf3f2368b
kimpoiprince <kimpoiprince@gmail.com>
2021-11-18 21:18:05 +0800

    product20211118

--- src/configs/config.js
+++ src/configs/config.js
@@ -7,3 +7,20 @@ export const LABELIMGS={
     家庭:"img/index/family-tag.png",
     策略:"img/index/trag-tag.png",
 }
+export const typecolor = {
+  家庭: "type10",
+  策略: "type11",
+  卡牌: "type12",
+};
+
+export const typecolor2 = {
+  家庭: "type20",
+  策略: "type21",
+  卡牌: "type22",
+};
+
+export const typecolor3 = {
+  家庭: "type30",
+  策略: "type31",
+  卡牌: "type32",
+};
