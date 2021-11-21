4357c5785771d1a26d31e69d232b15a36944c15f
yuki-crystal <bloodpinna@gmail.com>
2021-11-21 14:15:51 +0800

    新增問題

--- src/pages/customer_service/create_question.js
+++ src/pages/customer_service/create_question.js
@@ -2,13 +2,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Typeahead } from "react-bootstrap-typeahead"; // ES2015
 import "../../css/customer_service_message.css";
 import React, { useState } from "react";
-import { Link, useRouteMatch } from "react-router-dom";
 import Form from "react-bootstrap/Form";
+import { API_URL } from "../../configs/config";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+
+const categoryHierarchy = {
+  "商店相關":["商店相關2"],
+  "訂單進度":["訂單進度2"],
+  "退換維修相關問題":[],
+  "會員相關":[],
+  "折價卷":[],
+  "發票與退款":[],
+}
+
 const CreateQuestion = () => {
-  const { path, url } = useRouteMatch();
-  const [mainCategory, setMainCategory] = useState([]);
-  const [subCategory, setSubCategory] = useState([]);
-  const categoryOptions = [
+  const history = useHistory();
+  const [mainCategory, setMainCategory] = useState("");
+  const [subCategory, setSubCategory] = useState("");
+  const [questionContent, setQuestionContent] = useState("");
+  const mainCategoryOptions = [
     "商店相關",
     "訂單進度",
     "退換維修相關問題",
@@ -16,6 +30,42 @@ const CreateQuestion = () => {
     "折價卷",
     "發票與退款",
   ];
+  const [subCategoryOptions, setSubCategoryOptions] = useState([]);
+  const selecltMainCategory = (e) => {
+    setMainCategory(e);
+    if(mainCategory.length ===0){
+      return;
+    }
+    const mainCategoryText = mainCategory[0]
+    setSubCategoryOptions(categoryHierarchy[mainCategoryText])
+  };
+
+  const createQuestion = async () => {
+    if (mainCategory.length === 0) {
+      alert("請選擇主類別");
+      return;
+    }
+    if (subCategory.length === 0) {
+      alert("請選擇次類別");
+      return;
+    }
+    if (questionContent.length === 0) {
+      alert("請輸入內容");
+      return;
+    }
+    const postQuestionBody = {
+      user_id: 1,
+      category: mainCategory[0],
+      subcategory: subCategory[0],
+      content: questionContent,
+    };
+    await axios.post(`${API_URL}/cutomerService/questions`, postQuestionBody);
+    history.replace({
+      pathname: "record",
+      state: { isActive: true },
+    });
+  };
+
   return (
     <>
       {/* 問題類別 */}
@@ -25,8 +75,8 @@ const CreateQuestion = () => {
           <Typeahead
             id="basic-typeahead-single"
             labelKey="name"
-            onChange={setMainCategory}
-            options={categoryOptions}
+            onChange={selecltMainCategory}
+            options={mainCategoryOptions}
             placeholder="Choose a state..."
             selected={mainCategory}
           />
@@ -35,10 +85,10 @@ const CreateQuestion = () => {
           <Typeahead
             id="basic-typeahead-single"
             labelKey="name"
-            onChange={setMainCategory}
-            options={categoryOptions}
+            onChange={setSubCategory}
+            options={subCategoryOptions}
             placeholder="Choose a state..."
-            selected={mainCategory}
+            selected={subCategory}
           />
         </Form.Group>
       </div>
@@ -46,16 +96,18 @@ const CreateQuestion = () => {
       <div>
         <textarea
           class="problemItemOuter"
+          value={questionContent}
+          onChange={(e) => setQuestionContent(e.target.value)}
           placeholder="請填寫問題內容(限填200字)"
           required
           maxlength="200"
         ></textarea>
       </div>
-      <Link to={url.replace("create", "record")}>
-        <button class="submitButton">
-          <span>送出</span>
-        </button>
-      </Link>
+      {/* <Link to={url.replace("create", "record")}> */}
+      <button onClick={createQuestion} class="submitButton">
+        <span>送出</span>
+      </button>
+      {/* </Link> */}
       <p>
         <span class="remind">您的訊息，商店將會盡快為您進行回覆。</span>
       </p>
