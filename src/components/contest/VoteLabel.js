1ae699b44d8d77b69910047197ac381d43ed7b5a
Ya-Wen,Chan <89444541+YawenChan@users.noreply.github.com>
2021-11-24 18:31:48 +0800

    投票頁面功能更新

--- src/components/contest/VoteLabel.js
+++ src/components/contest/VoteLabel.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import {React,useState} from 'react'
 import PropTypes from 'prop-types'
 import {Container, Row,Col,Form} from 'react-bootstrap';
 
 function VoteLabel(props) {
-    const {name,vote} = props
+    const {name,vote} = props;
+    // const [count,setCount]=useState(vote);
+
+    // console.log("子元素內的值")
+    // console.log({name})
+    // console.log(count)
 
     return (
         <>
-        <label className="inputFont mb-3"><input type="radio" name="vote" className="conInput"
-        onClick={(e) =>{
-            // console.log({name})
-            // console.log({vote})
+        <label className="inputFont mb-3">
+        <input type="radio" name="vote" className="conInput"
+        value={name}
+        onClick={
+            (e)=>props.setVotename(e.target.value)
         }
-        }
+    
         />{name}</label> 
         </>
     )
